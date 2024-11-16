3f26f2c4b09be4885dc5907833950631eca2fb1a
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-16 11:18:03 -0300

    :sparkles: feat: Cria classe 'Cart'
    Classe 'Cart' vai tratar dos carrinhos em finalização

--- pages/models.py
+++ pages/models.py
@@ -15,6 +15,22 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(
+        max_length=20,
+        choices=(
+            ('open', 'Open'),
+            ('finalized', 'Finalized'),
+        ),
+        default='open',
+    )
+
+    def __str__(self):
+        return f'Cart #{self.id} - {self.user.username}'
+    
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
