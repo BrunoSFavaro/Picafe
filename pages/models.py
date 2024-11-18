24892d6094d2ea829d787dd25dadf5603d781523
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-18 13:50:33 -0300

    :sparkles: feat: Adiciona models Historico e Pedido
    Classes adicionada em pages/models

--- pages/models.py
+++ pages/models.py
@@ -44,3 +44,21 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f'{self.quantity} x {self.product.name}'
+    
+class Historico(models.Model):
+    pedido = models.ForeignKey('Pedido', on_delete=models.CASCADE, related_name='historico_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'PEdido #{self.id} - {self.quantity} x {self.product.name}'
+
+
+class Pedido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'Pedido #{self.id} - {self.user.username}'
